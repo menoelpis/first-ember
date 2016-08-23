edc8c239cf84a5afa989d0dc5bef182999ca8b94
Daniel Park <menoelpis@gmail.com>
2016-08-23 20:29:40 +0900

    Added Pages with application.hbs

--- app/router.js
+++ app/router.js
@@ -7,6 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  //this.route('about', {path:'about'}); # can be shorter if path and route matches
+  this.route('about');
+  this.route('services');
+  this.route('contact');
+
+  this.route('posts', {path:'/posts'}, function() {
+    this.route('new');
+    this.route('post', {path:':post_id'});
+  });
 });
 
 export default Router;
